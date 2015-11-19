c4c47c364a5dd1dbad7476d99be8ad7bc66cdb85
fujun <fujun@fujun-ThinkPad-Edge-E431.(none)>
2015-11-19 13:11:19 -0500

    "change union-find map into inner class"

--- graph/GLAs/CCompGLA.php
+++ graph/GLAs/CCompGLA.php
@@ -15,7 +15,7 @@ function ConnectedComponents(array $t_args, array $inputs, array $outputs)
 
     $sys_headers = ['unordered_map', 'set', 'vector'];
     $user_headers = [];
-    $lib_headers = ['UnionFindMap.h'];
+    $lib_headers = [];
 ?>
 
 using namespace std;
@@ -23,6 +23,74 @@ using namespace std;
 class <?=$className?>;
 
 class <?=$className?> {
+ 
+ class UnionFindMap{
+  private:
+    std::unordered_map<uint64_t, uint64_t> parent; 
+    std::unordered_map<uint64_t, uint64_t> sz;
+    const uint64_t NON_EXISTING_ID = -1;
+  public:
+    // constructor did nothing
+    UnionFindMap(){}
+
+    /*void Init(std::unordered_map<uint64_t, uint64_t>& parent){
+      this->parent = parent;
+    }*/
+
+    uint64_t Find(uint64_t i){
+      if (parent.find(i) == parent.end()){
+        return NON_EXISTING_ID;
+      }
+      // use path compression here
+      while (i != parent[i]){
+        parent[i] = parent[parent[i]];
+        i = parent[i];
+      }
+      return i;
+    }
+
+    // put merge small tree into higher tree
+    // if disjoint, merge and return false
+    void Union(uint64_t i, uint64_t j){
+      uint64_t ip = Find(i);
+      uint64_t jp = Find(j);
+
+      if (ip != NON_EXISTING_ID && jp != NON_EXISTING_ID){// both exists
+        if (ip != jp){
+          if (sz[ip] < sz[jp]){
+            parent[ip] = jp; sz[jp] += sz[ip];
+          }else{
+            parent[jp] = ip; sz[ip] += sz[jp];
+          }
+        }
+      }else if(ip == NON_EXISTING_ID && jp == NON_EXISTING_ID){// both new
+        parent[i] = i; sz[i] = 2;
+        parent[j] = i; 
+      }else if (jp == NON_EXISTING_ID){ // i exists
+        parent[j] = ip; sz[ip] ++;
+      }else{
+        parent[i] = jp; sz[jp] ++;
+      }
+    }
+
+    std::unordered_map<uint64_t, uint64_t>& GetUF(){
+      return parent;
+    }
+
+    // 
+    void FinalizeRoot(){
+      for(std::unordered_map<uint64_t, uint64_t>::iterator it = parent.begin(); it != parent.end(); ++ it){
+        it->second = Find(it->first);
+      }
+    }
+
+    void Clear(){
+      parent.clear();
+      sz.clear();
+    }
+
+ };
+
  private:
   // union-find map data structure, which contains nodeID->compID information
   UnionFindMap localUF, globalUF;
