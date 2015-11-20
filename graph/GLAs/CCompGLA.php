05276c8481971bfa59fe166191349eee233aaf28
fujun <fujun@fujun-ThinkPad-Edge-E431.(none)>
2015-11-20 17:15:16 -0500

    Add pointer to map structure During merge, check size of both. Make sure "this" is "larger" than the other. Swap pointer if necessary.

--- graph/GLAs/CCompGLA.php
+++ graph/GLAs/CCompGLA.php
@@ -26,26 +26,24 @@ class <?=$className?> {
  
  class UnionFindMap{
   private:
-    mct::closed_hash_map<uint64_t, uint64_t> parent; 
+    mct::closed_hash_map<uint64_t, uint64_t>* parent; 
     mct::closed_hash_map<uint64_t, uint64_t> sz;
 
     const uint64_t NON_EXISTING_ID = -1;
   public:
     // constructor did nothing
-    UnionFindMap(){}
-
-    /*void Init(mct::closed_hash_map<uint64_t, uint64_t>& parent){
-      this->parent = parent;
-    }*/
+    UnionFindMap(){
+      parent = new mct::closed_hash_map<uint64_t, uint64_t>();
+    }
 
     uint64_t Find(uint64_t i){
-      if (parent.find(i) == parent.end()){
+      if ((*parent).find(i) == (*parent).end()){
         return NON_EXISTING_ID;
       }
       // use path compression here
-      while (i != parent[i]){
-        parent[i] = parent[parent[i]];
-        i = parent[i];
+      while (i != (*parent)[i]){
+        (*parent)[i] = (*parent)[(*parent)[i]];
+        i = (*parent)[i];
       }
       return i;
     }
@@ -59,34 +57,46 @@ class <?=$className?> {
       if (ip != NON_EXISTING_ID && jp != NON_EXISTING_ID){// both exists
         if (ip != jp){
           if (sz[ip] < sz[jp]){
-            parent[ip] = jp; sz[jp] += sz[ip];
+            (*parent)[ip] = jp; sz[jp] += sz[ip];
           }else{
-            parent[jp] = ip; sz[ip] += sz[jp];
+            (*parent)[jp] = ip; sz[ip] += sz[jp];
           }
         }
       }else if(ip == NON_EXISTING_ID && jp == NON_EXISTING_ID){// both new
-        parent[i] = i; sz[i] = 2;
-        parent[j] = i; 
+        (*parent)[i] = i; sz[i] = 2;
+        (*parent)[j] = i; 
       }else if (jp == NON_EXISTING_ID){ // i exists
-        parent[j] = ip; sz[ip] ++;
+        (*parent)[j] = ip; sz[ip] ++;
       }else{
-        parent[i] = jp; sz[jp] ++;
+        (*parent)[i] = jp; sz[jp] ++;
       }
     }
 
-    mct::closed_hash_map<uint64_t, uint64_t>& GetUF(){
+    mct::closed_hash_map<uint64_t, uint64_t>* GetUF(){
       return parent;
+    }
+
+    bool IsEmpty(){
+      return (*parent).empty();
+    }
+
+    uint64_t GetSize(){
+      return (uint64_t) (*parent).size(); 
+    }
+
+    void SetData(mct::closed_hash_map<uint64_t, uint64_t>* other_data){
+      parent = other_data;
     }
 
     // 
     void FinalizeRoot(){
-      for(mct::closed_hash_map<uint64_t, uint64_t>::iterator it = parent.begin(); it != parent.end(); ++ it){
+      for(mct::closed_hash_map<uint64_t, uint64_t>::iterator it = (*parent).begin(); it != (*parent).end(); ++ it){
         it->second = Find(it->first);
       }
     }
 
     void Clear(){
-      parent.clear();
+      (*parent).clear();
       sz.clear();
     }
 
@@ -111,40 +121,54 @@ class <?=$className?> {
   void AddState(<?=$className?> &other) {
       FinalizeLocalState();
       other.FinalizeLocalState();
-      mct::closed_hash_map<uint64_t, uint64_t>& compIDLocal = localUF.GetUF();
-      mct::closed_hash_map<uint64_t, uint64_t>& otherState = other.localUF.GetUF();
+      mct::closed_hash_map<uint64_t, uint64_t>* compIDLocal = localUF.GetUF();
+      mct::closed_hash_map<uint64_t, uint64_t>* otherState = other.localUF.GetUF();
 
-      for(auto const& entry:otherState){
-        if (compIDLocal.find(entry.first) != compIDLocal.end()
-            && compIDLocal[entry.first] != entry.second){ // merge needed
-          globalUF.Union(compIDLocal[entry.first], entry.second);
+      if (localUF.GetSize() < other.localUF.GetSize()){
+        mct::closed_hash_map<uint64_t, uint64_t>* tmp = compIDLocal;
+        compIDLocal = otherState;
+        otherState = tmp;
+
+        localUF.SetData(compIDLocal);
+        other.localUF.SetData(otherState);
+      }
+      
+      
+      for(auto const& entry:(*otherState)){
+        if ((*compIDLocal).find(entry.first) != (*compIDLocal).end()
+            && (*compIDLocal)[entry.first] != entry.second){ // merge needed
+          globalUF.Union((*compIDLocal)[entry.first], entry.second);
         }else{
-          compIDLocal[entry.first] = entry.second;
+          (*compIDLocal)[entry.first] = entry.second;
         }
+      }
+
+      if (globalUF.IsEmpty()){
+        return;
       }
 
       // merge local and global
       globalUF.FinalizeRoot();
-      mct::closed_hash_map<uint64_t, uint64_t>& compIDGlobal = globalUF.GetUF();
-      
-      for (auto& p:compIDLocal){
-        if (compIDGlobal.find(p.second) != compIDGlobal.end()){
-          p.second = compIDGlobal[p.second];
+      mct::closed_hash_map<uint64_t, uint64_t>* compIDGlobal = globalUF.GetUF();
+
+      for (auto& p:(*compIDLocal)){
+        if ((*compIDGlobal).find(p.second) != (*compIDGlobal).end()){
+          p.second = (*compIDGlobal)[p.second];
         }
       }
       globalUF.Clear();
   }
 
   void FinalizeLocalState(){
-	if (!localFinalized){
-		localUF.FinalizeRoot();
-		localFinalized = true;
-	}
+  	if (!localFinalized){
+  		localUF.FinalizeRoot();
+  		localFinalized = true;
+  	}
   }
 
   void Finalize(){
-      OutputIterator = localUF.GetUF().begin();
-      EndOfOutput = localUF.GetUF().end();
+      OutputIterator = localUF.GetUF()->begin();
+      EndOfOutput = localUF.GetUF()->end();
   }
 
   bool GetNextResult(<?=typed_ref_args($outputs_)?>) {
