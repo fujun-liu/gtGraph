28bbdf6edf970e4e006f1617be82d2078e9069d0
fujun-liu <fujun-liu@users.noreply.github.com>
2015-12-04 14:31:29 -0500

    Update CCompUnionFindLP.R

--- gtGraph/R/CCompUnionFindLP.R
+++ gtGraph/R/CCompUnionFindLP.R
@@ -1,6 +1,6 @@
 CCompUnionFindLP <- function(data, inputs, outputs) {
     inputs <- convert.exprs(substitute(inputs))
     outputs <- convert.atts(substitute(outputs))
-    gla <- GLA(graph::CCompUnoinFindLP)
+    gla <- GLA(graph::CCompUnionFindLP)
     Aggregate(data, gla, inputs, outputs)
 }
